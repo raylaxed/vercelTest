b610f062f2f66831e8e1db18ac402e5cf4f20985
raylaxed <r.kohlprath@gmail.com>
2024-12-19 09:12:56 +0100

    font and bg

--- src/app/component/website-management/website-management.component.ts
+++ src/app/component/website-management/website-management.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-website-management',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './website-management.component.html',
   styleUrl: './website-management.component.css'
 })
 export class WebsiteManagementComponent {
-
+  // Add any component logic here
 }
